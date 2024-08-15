78ff415224715ceecfc7c4a6a785b9ab16c0342c
Somrita Banerjee <somritabanerjee@gmail.com>
2024-08-14 22:31:08 -0700

    Adding functionality to planner_scp_gusto_nodelet

--- mobility/planner_scp_gusto/src/planner_scp_gusto_nodelet.cc
+++ mobility/planner_scp_gusto/src/planner_scp_gusto_nodelet.cc
@@ -42,6 +42,9 @@
 #include <algorithm>
 #include <cmath>
 
+#define DEBUG true
+#define OUTPUT_DEBUG NODELET_DEBUG_STREAM
+
 /**
  * \ingroup planner
  */
@@ -83,6 +86,11 @@ class PlannerSCPGustoNodelet : public planner::PlannerImplementation {
     timer_d_ = nh->createTimer(
       ros::Duration(ros::Rate(DEFAULT_DIAGNOSTICS_RATE)),
         &PlannerSCPGustoNodelet::DiagnosticsCallback, this, false, true);
+    // Create a new optimization problem
+    top = new scp::TOP(10., 201);
+    top->Solve();
+    // Save node handle
+    nh_ = nh;
     // Save the epsilon value
     epsilon_ = cfg_.Get<double>("epsilon");
       // Notify initialization complete
@@ -102,11 +110,17 @@ class PlannerSCPGustoNodelet : public planner::PlannerImplementation {
     return true;
   }
 
-  void PlanCallback(ff_msgs::PlanGoal const& goal) {
-    // Do some basic error checks
+  void PlanCallback(ff_msgs::PlanGoal const &goal) override {
     ff_msgs::PlanResult plan_result;
     const std::vector<geometry_msgs::PoseStamped> &states = goal.states;
-    ROS_ERROR_STREAM("SB Got states from plancallback");
+
+    if (!LoadActionParams(goal)) {
+      ROS_ERROR_STREAM("Planner params are bad");
+      ff_msgs::PlanResult plan_result;
+      plan_result.response = RESPONSE::BAD_ARGUMENTS;
+      PlanResult(plan_result);
+      return;
+    }
 
     scp::Vec13 x0, xg;
     x0.segment(0, 3) << states.front().pose.position.x, states.front().pose.position.y,
@@ -123,29 +137,66 @@ class PlannerSCPGustoNodelet : public planner::PlannerImplementation {
       states.back().pose.orientation.z, states.back().pose.orientation.w;
     xg.segment(10, 3) << 0, 0, 0;
 
-    // Temporarily make a fake setpoint to stay in same position and add to plan_result
-    geometry_msgs::PoseStamped state = states.front();
-    ff_util::Setpoint sp;
-    sp.when = state.header.stamp;
-    sp.pose.position = state.pose.position;
-    sp.pose.orientation = state.pose.orientation;
-    geometry_msgs::Vector3 zeros;
-    zeros.x = 0.0; zeros.y = 0.0; zeros.z = 0.0;
-    sp.twist.linear = zeros;
-    sp.twist.angular = zeros;
-    sp.accel.linear = zeros;
-    sp.accel.angular = zeros;
-    plan_result.segment.push_back(sp);
+    // double candidate_Tf = 2*ChooseFinalTime(states.front(), states.back());
 
-    ROS_WARN_STREAM("The segment added was "<< state.pose.position.x);
+    // get sample increment
+    double radius;
+    if (!cfg_.Get<double>("robot_radius", radius)) radius = 0.26;
 
-    plan_result.response = RESPONSE::SUCCESS;
+    // try to get zones
+    std::vector<ff_msgs::Zone> zones;
+    if (!load_map()) {
+      ROS_ERROR("Planner failed to load keepins and keepouts");
+      return;
+    }
 
-    // Special case: we might already be there
-    if (plan_result.segment.size() < 2)
-      plan_result.response = RESPONSE::ALREADY_THERE;
-    // Callback with the result
-    return PlanResult(plan_result);
+    // Check if already at goal state
+    scp::Vec3 diff;
+    diff << std::abs(xg(0)-x0(0)),
+            std::abs(xg(1)-x0(1)),
+            std::abs(xg(2)-x0(2));
+    diff -= scp::Vec3::Ones() * map_res_;
+    if (diff(0) < 0 && diff(1) < 0 && diff(2) < 0) {
+     ROS_INFO_STREAM("Start and goal are within map resolution: " << diff.transpose());
+    }
+
+    // update solver settings
+    top->x0 = x0;
+    top->xg = xg;
+    top->radius_ = radius;
+
+    /*
+    if (candidate_Tf > top->Tf) {
+      top->Tf = std::max(max_time_, candidate_Tf);
+      top->dh = top->Tf / (top->N-1);
+      top->UpdateDynamics();
+      ROS_ERROR_STREAM("Problem Tf updated to : " << top->Tf);
+      // size_t new_N = static_cast<size_t>(1+std::ceil(top->Tf / 0.1));
+      // top->UpdateProblemDimension(new_N);
+    }
+    */
+
+    // solve
+    ROS_ERROR_STREAM("Starting SCP");
+    // auto start = std::chrono::high_resolution_clock::now();
+    bool is_solved = top->Solve();
+    // auto stop = std::chrono::high_resolution_clock::now();
+    // auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop-start);
+    // std::cout << "Solver worked in " << duration.count()/1e6 << "s!" << std::endl;
+
+    std::string pass = is_solved ? "solved" : "failed";
+    OUTPUT_DEBUG(
+        "PlannerSCP: Planner::SCP: Finished solving with status: " << pass);
+        // << " in " << duration.count()/1e6 << "s!");
+
+    if (is_solved) {
+      sample_trajectory(&plan_result.segment);
+      plan_result.response = RESPONSE::SUCCESS;
+      NODELET_FATAL_STREAM("Returning plan");
+    } else {
+      ROS_ERROR_STREAM("SCP::Planner failed to find solution!");
+    }
+    PlanResult(plan_result);
   }
 
   // Called to interrupt the process
@@ -154,7 +205,172 @@ class PlannerSCPGustoNodelet : public planner::PlannerImplementation {
  protected:
   ff_util::ConfigServer cfg_;
   ros::Timer timer_d_;
-  double min_control_period_;
+  double planner_dt_{0.5};  // how often to sample trajectory for output
+  double map_res_{0.5};     // map resolution
+
+ private:
+  std::vector<Eigen::AlignedBox3d> keep_in_zones_;
+  std::vector<Eigen::AlignedBox3d> keep_out_zones_;
+
+  void sample_trajectory(std::vector<ff_msgs::ControlState> *controls) {
+    size_t N = top->N;
+    scp::decimal_t dh = top->dh;
+
+    top->PolishSolution();    // ensure quaternions are normalized
+
+    scp::decimal_t mass = top->mass;
+    scp::Mat3 J = top->J;
+    scp::Mat3 Jinv = top->Jinv;
+
+    for (size_t ii = 0; ii < top->N-1; ii++) {
+      ff_msgs::ControlState state;
+      state.when = ros::Time(ii*dh);
+
+      scp::Vec3 F = top->Uprev[ii].segment(0, 3);
+      scp::Vec3 M = top->Uprev[ii].segment(3, 3);
+      scp::Vec3 omega = top->Xprev[ii].segment(10, 3);
+      scp::Vec3 accel = 1/mass * F;
+      scp::Vec3 alpha = Jinv * (M - omega.cross(J*omega));
+
+      // Linear state
+      state.pose.position.x = top->Xprev[ii](0);
+      state.pose.position.y = top->Xprev[ii](1);
+      state.pose.position.z = top->Xprev[ii](2);
+
+      state.twist.linear.x = top->Xprev[ii](3);
+      state.twist.linear.y = top->Xprev[ii](4);
+      state.twist.linear.z = top->Xprev[ii](5);
+
+      state.accel.linear.x = accel(0);
+      state.accel.linear.y = accel(1);
+      state.accel.linear.z = accel(2);
+
+      // Rotational state
+      state.pose.orientation.x = top->Xprev[ii](6);
+      state.pose.orientation.y = top->Xprev[ii](7);
+      state.pose.orientation.z = top->Xprev[ii](8);
+      state.pose.orientation.w = top->Xprev[ii](9);
+
+      state.twist.angular.x = omega(0);
+      state.twist.angular.y = omega(1);
+      state.twist.angular.z = omega(2);
+
+      state.accel.angular.x = alpha(0);
+      state.accel.angular.y = alpha(1);
+      state.accel.angular.z = alpha(2);
+
+      controls->push_back(state);
+    }
+
+    // Final state has 0 velocity and acceleration
+    ff_msgs::ControlState state;
+    state.when = ros::Time(dh*(N-1));
+    state.pose.position.x = top->Xprev[N-1](0);
+    state.pose.position.y = top->Xprev[N-1](1);
+    state.pose.position.z = top->Xprev[N-1](2);
+    state.pose.orientation.x = top->Xprev[N-1](6);
+    state.pose.orientation.y = top->Xprev[N-1](7);
+    state.pose.orientation.z = top->Xprev[N-1](8);
+    state.pose.orientation.w = top->Xprev[N-1](9);
+    controls->push_back(state);
+
+    NODELET_FATAL_STREAM("Done packing controls");
+  }
+
+  bool load_map() {
+    if (!cfg_.Get<double>("map_resolution", map_res_)) map_res_ = 0.5;
+
+    double radius;
+    if (!cfg_.Get<double>("robot_radius", radius)) radius = 0.26;
+
+    std::vector<ff_msgs::Zone> zones;
+    bool got = GetZones(zones);
+    if (!got) return false;
+
+    // Global min and max of map
+    Eigen::Vector3d min, max;
+    min << 1000.0, 1000.0, 1000.0;
+    max << -1000.0, -1000.0, -1000.0;
+
+    Eigen::Vector3d zmin, zmax;   // Min and max of each zone
+    Eigen::AlignedBox3d temp;
+
+    // Iterate through each zone and save
+    for (auto &zone : zones) {
+      zmin << std::min(zone.min.x, zone.max.x),
+          std::min(zone.min.y, zone.max.y), std::min(zone.min.z, zone.max.z);
+      zmax << std::max(zone.min.x, zone.max.x),
+          std::max(zone.min.y, zone.max.y), std::max(zone.min.z, zone.max.z);
+
+      if (zone.type == ff_msgs::Zone::KEEPIN) {
+        // Update global min and max of map
+        for (int ii = 0; ii < 3; ii++) {
+          //  Need to check across all combinations
+          //  since some JSON params have flipped corners
+          min(ii) = std::min(min(ii), zmin(ii));
+          min(ii) = std::min(min(ii), zmax(ii));
+          max(ii) = std::max(max(ii), zmin(ii));
+          max(ii) = std::max(max(ii), zmax(ii));
+        }
+
+        temp.extend(Eigen::Vector3d(zmin(0), zmin(1), zmin(2)));
+        temp.extend(Eigen::Vector3d(zmax(0), zmax(1), zmax(2)));
+        keep_in_zones_.push_back(temp);
+      } else {
+        // Inflate obstacles by Astrobee radius
+        zmin << zmin(0) - radius, zmin(1) - radius, zmin(2) - radius;
+        zmax << zmax(0) + radius, zmax(1) + radius, zmax(2) + radius;
+
+        temp.extend(Eigen::Vector3d(zmin(0), zmin(1), zmin(2)));
+        temp.extend(Eigen::Vector3d(zmax(0), zmax(1), zmax(2)));
+        keep_out_zones_.push_back(temp);
+      }
+    }
+
+    // Update position bds for solver
+    for (size_t ii = 0; ii < 3; ii++) {
+      top->pos_min_(ii) = min(ii);
+      top->pos_max_(ii) = max(ii);
+    }
+
+    if (keep_in_zones_.size() == 0) {
+      ROS_ERROR("Zero keepin zones!! Plan failed");
+      return false;
+    }
+
+    return true;
+  }
+
+  bool LoadActionParams(const ff_msgs::PlanGoal &goal) {
+    // Planner params
+    faceforward_ = goal.faceforward;
+    check_obstacles_ = goal.check_obstacles;
+    desired_vel_ = goal.desired_vel;
+    desired_accel_ = goal.desired_accel;
+    desired_omega_ = goal.desired_omega;
+    desired_alpha_ = goal.desired_alpha;
+    max_time_ = goal.max_time.toSec();
+    control_rate_ = goal.desired_rate;
+
+    // Solver params
+    top->faceforward_     = faceforward_;
+    top->desired_vel_     = desired_vel_;
+    top->desired_accel_   = desired_accel_;
+    top->desired_omega_   = desired_omega_;
+    top->desired_alpha_   = desired_alpha_;
+    top->max_time_        = max_time_;
+    top->control_rate_    = control_rate_;
+    top->UpdateStateCons();
+    top->UpdateVelCons();
+    top->UpdateAngVelCons();
+
+    // check validity of params
+    if (control_rate_ == 0.0 || desired_vel_ == 0.0 || desired_accel_ == 0.0 ||
+        desired_omega_ == 0.0 || desired_alpha_ == 0.0) {
+      return false;
+    }
+    return true;
+  }
 };
 
 PLUGINLIB_EXPORT_CLASS(planner_scp_gusto::PlannerSCPGustoNodelet,
