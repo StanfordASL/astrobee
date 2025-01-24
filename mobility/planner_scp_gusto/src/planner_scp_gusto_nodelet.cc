1e590920d3878794124f08661c207d57b2dc98c4
Somrita Banerjee <somritabanerjee@gmail.com>
2025-01-24 11:21:48 -0800

    changed granite z constraints

--- mobility/planner_scp_gusto/src/planner_scp_gusto_nodelet.cc
+++ mobility/planner_scp_gusto/src/planner_scp_gusto_nodelet.cc
@@ -251,8 +251,8 @@ class PlannerSCPGustoNodelet : public planner::PlannerImplementation {
     top->enforce_obs_avoidance_const = enforce_obs_avoidance_const_;
     top->is_granite = is_granite_;
     if (top->is_granite) {
-      top->x_min(2) = -0.675;  // z coordinate
-      top->x_max(2) = -0.67;
+      top->x_min(2) = -0.80;  // z coordinate
+      top->x_max(2) = -0.65;
       top->x_min(6) = -0.05;  // qx
       top->x_max(6) = 0.05;
       top->x_min(7) = -0.05;  // qy
